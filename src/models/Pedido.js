1f41ceb2c952984be32839dae46277493e3285da
juguarnieri <juliaguarnieri04@gmail.com>
2025-02-11 11:51:25 -0300

    feat: create files

---
+++ src/models/Pedido.js
@@ -0,0 +1,12 @@
+const { v4: uuid4 } = require("uuid");
+class Movie {
+    constructor(title, actor, duration, gender = "Unknown") {
+        this.id = uuid4();
+        this.title = title;
+        this.actor = actor;  
+        this.duration = duration;
+        this.gender = gender;  
+    }
+}
+
+module.exports = Movie;
