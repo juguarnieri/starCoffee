3eb354e2b7320ae075d24e03910ef86560ed569c
juguarnieri <juliaguarnieri04@gmail.com>
2025-02-12 20:13:37 -0300

    feat: add getPedidoById method to PedidoLista

--- src/models/PedidoLista.js
+++ src/models/PedidoLista.js
@@ -10,6 +10,19 @@ class PedidoLista {
       this.pedidos.push(pedido);
       return pedido;
     }
+  
+    getPedidoById(id) {
+      const pedido = this.pedidos.find((pedido) => pedido.id == id);
+      if (!pedido) {
+        throw new Error("Pedido não encontrado");
+      }
+      return {
+        id: pedido.id,
+        itens: pedido.itens,
+        status: pedido.status,
+      };
+    }
+  
 };
   module.exports = PedidoLista;
   
